a81b7c204e4d9a35e4d16110fcf0841209f739cf
Masuma01~ <javariamasooma.bscssef20@iba-suk.edu.pk>
2023-11-14 13:03:31 +0500

    made some changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -16,6 +17,10 @@ const Header = () => {
             <Nav.Link href="#pricing"> Activity </Nav.Link>
             <Nav.Link href="#pricing">How it Works </Nav.Link>
           
+          </Nav>
+          <Nav className='me-auto'>
+            <Button variant="secondary">Create</Button>
+            <Button variant="info">Sign In</Button>
           </Nav>
         </Container>
       </Navbar>
